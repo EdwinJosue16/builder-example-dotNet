042f49aed8d238c67134b363ae6caa48fb7bccd2
Edwin Josué Brenes <31810694+EdwinJosue16@users.noreply.github.com>
2021-05-22 08:47:51 -0600

    Add files via upload
    First commit

---
+++ builder-example/Controllers/CalculadoraIMCController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Web.Mvc;
+using Laboratorio3.Models;
+
+namespace Laboratorio3.Controllers
+{
+    public class CalculadoraIMCController : Controller
+    {
+        
+        public ActionResult ResultadoIMC()
+        {
+            PersonaModel[] personas = new PersonaModel[30];
+            Random rnd = new Random();
+
+            for (int i = 0; i < personas.Length; ++i)
+            {
+                //personas[i] = new PersonaModel.PersonaModelBuilder(i).setEdadMayor(18).setSalario(1500000);
+                //.setPeso(rnd.NextDouble() * (150.0 - 20.0) + 20.0 ).setEstatura(rnd.NextDouble() * (2.0 - 1.0) + 1.0).build();
+
+                personas[i].IMC = personas[i].Peso / (personas[i].Estatura * personas[i].Estatura);
+            }
+
+
+
+            ViewBag.personas = personas;
+
+            return View();
+        }
+    }
+}
---
+++ builder-example/Models/PersonaModel.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laboratorio3.Models
+{
+    public class PersonaModel
+    {
+        //Propiedades: cuando se instancia una persona se puede hacer persona.Peso=valor
+
+        public string Nombre { get; set; }
+        public int Id { get; set; }
+        public double Peso { get; set;}
+        public double Estatura { get; set; }
+        public double IMC { get; set; }
+        public double salario { get; set; }
+        public int edad { get; set; }
+        public string escuela { get; set; }
+
+        //Constructores (no es indispensable en este caso)
+
+        //public PersonaModel() {
+        //    this.Nombre = "Sin nombre";
+        //    this.Id = 0;
+        //    this.Peso = 0.0;
+        //    this.Estatura = 0.0;
+        //    this.IMC = 0.0;
+        //}
+
+        //public PersonaModel(int Id, string Nombre,double Peso, double Estatura, double IMC)
+        //{
+        //    this.Nombre = Nombre;
+        //    this.Id = Id;
+        //    this.Peso = Peso;
+        //    this.Estatura = Estatura;
+        //    this.IMC = IMC;
+        //}
+
+        private PersonaModel() {
+
+        }
+
+        public class PersonaModelBuilder{
+            public PersonaModel persona;
+
+            public class PersonaModelBuilderMayor {
+
+                public PersonaModel personaMayor;
+
+                public PersonaModelBuilderMayor(int edad) {
+                    personaMayor.edad = edad;
+                }
+                public PersonaModelBuilderMayor setSalario(double salario) {
+                    personaMayor.salario = salario;
+                    return this;
+                }
+
+               
+            }
+
+            public PersonaModelBuilder(int Id) {
+                persona = new PersonaModel();
+                persona.Id = Id;
+            }
+
+            public PersonaModelBuilder setNombre(string nombre) {
+                persona.Nombre = nombre;
+                return this;
+            }
+
+            public PersonaModelBuilder setPeso(double peso) {
+                persona.Peso=peso;
+                return this;
+            }
+
+            public PersonaModelBuilder setEstatura(double estatura) {
+                persona.Estatura=estatura;
+                return this;
+            }
+
+            public PersonaModelBuilderMayor setEdadMayor(int edad) {
+                PersonaModelBuilderMayor personaMayor = new PersonaModelBuilderMayor(edad);
+                return personaMayor;
+            }
+
+            public PersonaModel build() {
+                return persona;
+            }
+
+            
+        } 
+    }
+}
